33ddf3b53b5cd13461344060a57ab199216f8ff5
Stephane Meaudre <smeaudre@kaliop.com>
2022-02-04 12:02:21 +0100

    Add skeleton class + command

--- IbexaContentDto/src/Command/CreateContentDtoCommand.php
+++ IbexaContentDto/src/Command/CreateContentDtoCommand.php
@@ -17,6 +17,8 @@ class CreateContentDtoCommand extends Command
 {
     protected static $defaultName = 'kaliop:dto:create';
 
+    private QuestionHelper $questionHelper;
+
     use IbexaServicesTrait;
 
     /**
@@ -33,11 +35,12 @@ class CreateContentDtoCommand extends Command
     /**
      * @param $repository
      */
-//    public function __construct($repository)
-//    {
-//        $this->repository = $repository;
-//        parent::__construct();
-//    }
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+        $this->questionHelper = $this->get('question');
+        parent::__construct();
+    }
 
     /**
      * @param InputInterface $input
@@ -48,8 +51,13 @@ class CreateContentDtoCommand extends Command
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         // Step 1 : list all content-type identifier
+        $contentTypeIdentifierSelected = $input->getArgument('content_type') ?? $this->getContentType();
+        if (is_null($contentTypeIdentifierSelected)) {
+            return Command::fail;
+        }
 
         // Step 2 : list fields of content type selected
+        $contentType = $this->repository->getContentTypeService()->loadByIdentifier($contentTypeIdentifierSelected);
 
         // Step 3 : build DTO class
 
@@ -59,4 +67,22 @@ class CreateContentDtoCommand extends Command
     }
 
 
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     *
+     * @return string
+     */
+    private function getContentType(InputInterface $input, OutputInterface $output): string
+    {
+        $listContentTypes = $this->repository->getContentTypeService()->loadContentTypeList();
+        $question = new ChoiceQuestion('Select a content-type', $listContentTypes);
+
+        return $question->ask($input, $output, $question);
+    }
+
+    private function buildDtoClass(ContentType $contentType)
+    {
+
+    }
 }
